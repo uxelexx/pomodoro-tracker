7fdc311e9a699d64e5f592c617e4a5e41402f207
uxelexx <i@kkornilov.ru>
2023-12-20 17:08:27 +0300

    init

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: false,
+}
