d3838acee6c9fc0ceb5022f54a24603630d75f0d
uxelexx <i@kkornilov.ru>
2023-12-22 11:09:24 +0300

    constant & utils

---
+++ utils/constants.ts
@@ -0,0 +1,3 @@
+export const RADIUS = 40
+export const DASHOFFSET = 110
+export const DASHSTROKE = 360
---
+++ utils/index.ts
@@ -0,0 +1,14 @@
+import { DASHOFFSET, DASHSTROKE } from '@/constants'
+
+export function formatTime(time: number) {
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
+    2,
+    '0'
+  )}`
+}
+
+export function calcTick(seconds: number) {
+  return (DASHSTROKE - DASHOFFSET) / seconds
+}
