f2e3d5d02bee16041d56a113941fd7e8d24310cb
uxelexx <i@kkornilov.ru>
2023-12-22 11:09:46 +0300

    timer

---
+++ components/Timer/Timer.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState, useMemo } from 'react'
+import styles from './Timer.module.css'
+import { Progress } from '../Progress'
+import { formatTime, calcTick } from '@/utils'
+
+type PomodoroProps = {
+  seconds: number
+  isRunning: boolean
+}
+
+export function Timer() {
+  const [seconds, setSeconds] = useState<number>(10)
+  const [isRunning, setIsRunning] = useState<boolean>(true)
+  const [dashOffset, setDashOffset] = useState(0)
+  const requestRef = useRef<number | null>(null)
+  const startTimeRef = useRef<number>(0)
+  const tick = useMemo(() => calcTick(seconds), []) //memoize initial seconds tick value
+
+  const animate = (time: number) => {
+    if (!startTimeRef.current) {
+      startTimeRef.current = time
+    }
+
+    const elapsed = time - startTimeRef.current
+
+    if (elapsed >= 1000) {
+      // One second has passed
+      startTimeRef.current = time
+      if (seconds > 0) {
+        setSeconds(seconds - 1)
+        setDashOffset(dashOffset - tick)
+      } else {
+        // Pomodoro session is complete
+        setIsRunning(false)
+        setSeconds(0)
+        setDashOffset(0)
+        return
+      }
+    }
+
+    requestRef.current = requestAnimationFrame(animate)
+  }
+
+  useEffect(() => {
+    if (isRunning) {
+      requestRef.current = requestAnimationFrame(animate)
+    }
+
+    return () => {
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current)
+        requestRef.current = null
+      }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isRunning, seconds])
+
+  function handlePause() {
+    setIsRunning(!isRunning)
+    if (requestRef.current) {
+      cancelAnimationFrame(requestRef.current)
+      requestRef.current = null
+    }
+  }
+
+  return (
+    <article className={styles.circles}>
+      <div className={styles.outer}>
+        <div className={styles.inner}>
+          <Progress dashOffset={dashOffset} />
+          <p className={styles.timer}>{formatTime(seconds)}</p>
+          <button onClick={handlePause} className={styles.pause}>
+            <p>{!isRunning ? 'Start' : 'Pause'}</p>
+          </button>
+        </div>
+      </div>
+    </article>
+  )
+}
