351598667cc7bbaa40802be46d831e5ed3771ca1
uxelexx <i@kkornilov.ru>
2023-12-22 11:02:24 +0300

    progress circle

---
+++ components/Progress/Progress.tsx
@@ -0,0 +1,29 @@
+import styles from './Progress.module.css'
+
+import { DASHSTROKE, RADIUS } from '../Timer/Timer.tsx'
+
+type ProgressProps = {
+  dashOffset: number
+}
+
+export function Progress({ dashOffset }: ProgressProps) {
+  return (
+    <svg
+      className={styles.svg}
+      version="1.1"
+      id="circle"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 100 100"
+      xmlSpace="preserve"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        r={RADIUS}
+        strokeDashoffset={dashOffset}
+        strokeDasharray={DASHSTROKE.toString()}
+      ></circle>
+    </svg>
+  )
+}
