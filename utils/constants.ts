6998e167195384841c0e7dbe89647784744d8c24
uxelexx <i@kkornilov.ru>
2023-12-25 18:08:54 +0300

    make min-max const for timer

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
-import type { Colors, ColorsHex } from '@/types'
+import type { Colors, ColorsHex, MinMax, Timer } from '@/types'
 
 export const RADIUS = 40
 export const DASHOFFSET = 110
@@ -8,4 +8,19 @@ export const colors: Record<Colors, ColorsHex> = {
   red: '#f77272',
   blue: '#6488ea',
   purple: '#d980f8',
-}
+} as const
+
+export const maxMinTime: Record<Timer, MinMax> = {
+  pomodoro: {
+    max: 60,
+    min: 20,
+  },
+  'short-break': {
+    max: 10,
+    min: 5,
+  },
+  'long-break': {
+    max: 20,
+    min: 15,
+  },
+} as const
