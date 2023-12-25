6bdc1eb09bf8846fa7b96217de8905a89bba7c3b
uxelexx <i@kkornilov.ru>
2023-12-25 13:07:46 +0300

    *

--- utils/index.ts
+++ utils/index.ts
@@ -13,12 +13,15 @@ export function formatTime(time: number) {
 export function calcTick(seconds: number) {
   return (DASHSTROKE - DASHOFFSET) / seconds
 }
+
 export function getKeys<T extends object>(obj: T) {
   return Object.keys(obj) as (keyof T)[]
 }
+
 export function getEntries<T extends object>(obj: T) {
   return Object.entries(obj) as [keyof T, T[keyof T]][]
 }
+
 export function getAccentColor(color: Colors) {
   return colors[color]
 }
