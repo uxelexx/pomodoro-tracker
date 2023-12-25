5d31c0ed6ca9421bb1ca21148becf96c92a6d0b4
uxelexx <i@kkornilov.ru>
2023-12-25 12:59:10 +0300

    types

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export type ColorsHex = '#f77272' | '#70f2f7' | '#d980f8'
+export type Colors = 'red' | 'blue' | 'purple'
+
+export type Fonts = string
+
+export type Timer = 'pomodoro' | 'shortBreak' | 'longBreak'
+export type Durations = Record<Timer, number>
