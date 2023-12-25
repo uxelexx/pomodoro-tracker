6278c9767896a7365ed6203776a62a6122d967e3
uxelexx <i@kkornilov.ru>
2023-12-25 18:09:23 +0300

    min max type, rename Timer values

--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,9 @@ export type Colors = 'red' | 'blue' | 'purple'
 
 export type Fonts = string
 
-export type Timer = 'pomodoro' | 'shortBreak' | 'longBreak'
+export type Timer = 'pomodoro' | 'short-break' | 'long-break'
 export type Durations = Record<Timer, number>
+export type MinMax = {
+  max: number
+  min: number
+}
