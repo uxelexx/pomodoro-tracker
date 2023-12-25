81b1db5202662674fb777eaf78b28b4a58391399
uxelexx <i@kkornilov.ru>
2023-12-25 12:59:16 +0300

    colors const

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,11 @@
+import type { Colors, ColorsHex } from '@/types'
+
 export const RADIUS = 40
 export const DASHOFFSET = 110
 export const DASHSTROKE = 360
+
+export const colors: Record<Colors, ColorsHex> = {
+  red: '#f77272',
+  blue: '#70f2f7',
+  purple: '#d980f8',
+}
